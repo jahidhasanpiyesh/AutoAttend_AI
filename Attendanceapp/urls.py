5b804ad18b5e81cf96da5eab3f32ac198ddb57dc
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-09-03 01:24:48 +0600

    create a new html file and show sortcut load section.

--- Attendanceapp/urls.py
+++ Attendanceapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register_student/', views.register_student, name='register_student'),
 ]
     
 
--- Attendanceapp/views.py
+++ Attendanceapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def register_student(request):
+    return render(request, 'register_student.html')
